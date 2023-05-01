05f3d4605c1525c555809697dfd15d08178c5871
Chaabane fares <117437576+ChaabaneFares@users.noreply.github.com>
2023-05-01 11:16:46 +0100

    starting repo"
    d
    :c
    w

---
+++ pages/services.jsx
@@ -0,0 +1,11 @@
+import { FirstSection } from "./services/firstSection";
+import styles from "../styles/services.module.css"
+export default function services() {
+  return (
+    <main>
+      <section className={styles.FirstSection}>
+<FirstSection/>
+</section>
+    </main>
+  );
+}
---
+++ pages/services/firstSection.jsx
@@ -0,0 +1,10 @@
+
+
+export const FirstSection = () => {
+  return (
+    <div>
+        <h1>Move people</h1> 
+    <h1>drive results</h1>
+    </div>
+  )
+}
