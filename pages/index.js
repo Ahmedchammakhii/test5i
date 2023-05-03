9c607ee85b2af2d47a2336223b663d5b468ff78f
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-05-03 13:17:30 +0100

    menu & navbar responsiveness

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-
-
+import Layout from "./index/layout/layout"
+import { useState, useEffect } from "react"
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 764);
+    }
+    setIsMobile(window.innerWidth < 764);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isMobile]);
   return (
-   <>
-   </>
+    <Layout isMobile={isMobile}>
+
+    </Layout>
   )
 }
