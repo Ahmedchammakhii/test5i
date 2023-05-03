a81a5daa491da6449aa22805cccae00112be7e95
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-05-03 13:16:29 +0100

    layout

---
+++ pages/index/layout/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './navbar/navbar'
+export default function layout({ children, isMobile }) {
+    return (
+        <div>
+            <Navbar isMobile={isMobile} />
+            {children}
+        </div>
+    )
+}
