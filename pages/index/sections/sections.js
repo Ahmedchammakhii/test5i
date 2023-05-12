2eccb46eb79e562ab76355b2da0003cd1858e2b0
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-05-12 18:30:23 +0100

    fixed image croping

--- pages/index/sections/sections.js
+++ pages/index/sections/sections.js
@@ -10,7 +10,7 @@ export default function sections({ sticky, setSticky, preLast, last, element, in
         right_side: { width: "50%", position: "relative" },
         img: {
             clipPath: scroll ? scroll > (window.innerHeight * (0.25)) + (window.innerHeight * index) && !last ?
-                "inset(0px 0px " + (100 - (((window.innerHeight * 0.55) - (scroll - window.innerHeight * index)) / 292) * 100) + "%)"
+                "inset(0px 0px " + (100 - (((window.innerHeight * 0.70) - (scroll - window.innerHeight * index)) / 292) * 100) + "%)"
                 : "none" : "none",
             zIndex: 5 + Number("-" + index), position: last && sticky ? "sticky" : "fixed", top: "50%", transform: "translate(0, -50%)", width: "100%",
             width: last && sticky ? "100%" : `calc(${screen === "tablet" ? "78%" : "62%"} / 2)`, height: `calc(100vw * ${screen === "tablet" ? "0.78" : "0.62"} / 2)`, objectFit: "cover", borderRadius: "11%"
@@ -29,6 +29,7 @@ export default function sections({ sticky, setSticky, preLast, last, element, in
         } else if (preLast && (100 - (((window.innerHeight * 0.55) - (scroll - window.innerHeight * index)) / 292) * 100) < 99) {
             setSticky(false)
         }
+        console.log(scroll);
     }, [scroll]);
 
     return (
