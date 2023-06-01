567b63f2c290897254d67f3fab757c4dc15e28b9
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-06-01 19:09:24 +0100

    cards mouse change shape on hover

--- pages/landing/black-section/cards.js
+++ pages/landing/black-section/cards.js
@@ -67,12 +67,14 @@ export default function Cards({ screen, scroll, containerRef }) {
                     document.getElementsByClassName('header_lines_btn')[0].style.background = "black"
                     document.getElementsByClassName('header_lines_btn')[1].style.background = "black"
                     document.getElementsByClassName("primary-cursor")[0].style.background = "black"
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
                     document.getElementsByClassName('quote')[0].style.background = 'linear-gradient(to bottom right, transparent, #3398ff 80%)'
                     document.getElementsByClassName('logo')[0].src = 'https://cdn.discordapp.com/attachments/1073737355896299542/1110855540231381052/logo_noir_ver.png'
                 } else if (v[0] >= 0.6 && document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
                     document.getElementsByClassName('header_lines_btn')[0].style.background = "white"
                     document.getElementsByClassName('header_lines_btn')[1].style.background = "white"
                     document.getElementsByClassName("primary-cursor")[0].style.background = "white"
+                    document.getElementsByClassName("cursor-click")[0].style.color = "black"
                     document.getElementsByClassName('quote')[0].style.background = 'linear-gradient(312deg,#73ffa2, transparent 80%)'
                     document.getElementsByClassName('logo')[0].src = 'https://cdn.discordapp.com/attachments/1073737355896299542/1110855539631595521/logo_blanc_ver.png'
                 }
@@ -118,7 +120,32 @@ export default function Cards({ screen, scroll, containerRef }) {
     return (
         <div ref={container} style={{ width: "100%", height: screen.mobile ? '100vh' : '200vh', display: "flex", justifyContent: "center", position: "relative" }}>
             <div style={{ zIndex: 0, pointerEvents: "none", position: "absolute", height: "100%", transform: `translate(-50% , calc(-${cardsDimension}px * (0.12 * 1.5  )))`, left: `calc(50% + ((${cardsDimension}px * 0.49) - (${cardsDimension}px * 0.153) * 4.5))` }}>
-                <img alt='#' onClick={() => {
+                <img onMouseEnter={() => {
+                    if (document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+                    } else {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "black"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+
+                    }
+                }} onMouseLeave={() => {
+                    document.getElementsByClassName("primary-cursor")[0].style.width = "15px"
+                    document.getElementsByClassName("cursor-click")[0].textContent = ""
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                    document.getElementsByClassName("primary-cursor")[0].style.height = "12px"
+                    document.getElementsByClassName("primary-cursor")[0].style.opacity = 1
+                    document.getElementsByClassName("cursor-click")[0].style.visibility = "hidden"
+
+                }} alt='#' onClick={() => {
                     handleClick(0)
                 }} style={{
                     ...styles.cards, pointerEvents: "auto", objectFit: "cover", borderRadius: screen.mobile ? "1em" : "1.8em",
@@ -126,7 +153,32 @@ export default function Cards({ screen, scroll, containerRef }) {
                 }} src="https://uploads-ssl.webflow.com/63ffb1d8365f630bd05a80b9/640b1d744379a3bfb50eb8f0_sc5-p-800.jpg" />
             </div>
             <div style={{ zIndex: 1, pointerEvents: "none", position: "absolute", height: "100%", transform: `translate(-50% , calc(-${cardsDimension}px * (0.12 * 1.5  )))`, left: `calc(50% + ((${cardsDimension}px * 0.49) - (${cardsDimension}px * 0.153) * 3.1))` }}>
-                <img alt='#' onClick={() => {
+                <img onMouseEnter={() => {
+                    if (document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+                    } else {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "black"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+
+                    }
+                }} onMouseLeave={() => {
+                    document.getElementsByClassName("primary-cursor")[0].style.width = "15px"
+                    document.getElementsByClassName("cursor-click")[0].textContent = ""
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                    document.getElementsByClassName("primary-cursor")[0].style.height = "12px"
+                    document.getElementsByClassName("primary-cursor")[0].style.opacity = 1
+                    document.getElementsByClassName("cursor-click")[0].style.visibility = "hidden"
+
+                }} alt='#' onClick={() => {
                     handleClick(1)
                 }} style={{
                     ...styles.cards, pointerEvents: "auto", objectFit: "cover", borderRadius: screen.mobile ? "1em" : "1.8em",
@@ -134,7 +186,32 @@ export default function Cards({ screen, scroll, containerRef }) {
                 }} src="https://uploads-ssl.webflow.com/63ffb1d8365f630bd05a80b9/640b1d7c4481e4143bf2c422_sc6-p-800.jpg" />
             </div>
             <div style={{ zIndex: 2, pointerEvents: "none", position: "absolute", height: "100%", transform: `translate(-50% , calc(-${cardsDimension}px * (0.12 * 1.5  )))`, left: `calc(50% + ((${cardsDimension}px * 0.49) - (${cardsDimension}px * 0.153) * 2.08))` }}>
-                <img alt='#' onClick={(e) => {
+                <img onMouseEnter={() => {
+                    if (document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+                    } else {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "black"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+
+                    }
+                }} onMouseLeave={() => {
+                    document.getElementsByClassName("primary-cursor")[0].style.width = "15px"
+                    document.getElementsByClassName("cursor-click")[0].textContent = ""
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                    document.getElementsByClassName("primary-cursor")[0].style.height = "12px"
+                    document.getElementsByClassName("primary-cursor")[0].style.opacity = 1
+                    document.getElementsByClassName("cursor-click")[0].style.visibility = "hidden"
+
+                }} alt='#' onClick={(e) => {
                     handleClick(2)
                 }} style={{
                     ...styles.cards, pointerEvents: "auto", objectFit: "cover", borderRadius: screen.mobile ? "1em" : "1.8em",
@@ -142,7 +219,32 @@ export default function Cards({ screen, scroll, containerRef }) {
                 }} src="https://uploads-ssl.webflow.com/63ffb1d8365f630bd05a80b9/642071c9b4f0d2f23c0d0fd3_stewart-maclean-Zs1WKNa4Oy0-unsplash.jpg" />
             </div>
             <div style={{ zIndex: 3, pointerEvents: "none", position: "absolute", height: "100%", transform: `translate(-50% , calc(-${cardsDimension}px * 0.12))`, left: `calc(50% + ((${cardsDimension}px * 0.49) - ${cardsDimension}px * 0.153))` }}>
-                <img alt='#' onClick={() => {
+                <img onMouseEnter={() => {
+                    if (document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+                    } else {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "black"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+
+                    }
+                }} onMouseLeave={() => {
+                    document.getElementsByClassName("primary-cursor")[0].style.width = "15px"
+                    document.getElementsByClassName("cursor-click")[0].textContent = ""
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                    document.getElementsByClassName("primary-cursor")[0].style.height = "12px"
+                    document.getElementsByClassName("primary-cursor")[0].style.opacity = 1
+                    document.getElementsByClassName("cursor-click")[0].style.visibility = "hidden"
+
+                }} alt='#' onClick={() => {
                     handleClick(3)
                 }} style={{
                     ...styles.cards, pointerEvents: "auto", objectFit: "cover", borderRadius: screen.mobile ? "1em" : "1.8em",
@@ -150,7 +252,32 @@ export default function Cards({ screen, scroll, containerRef }) {
                 }} src="https://uploads-ssl.webflow.com/63ffb1d8365f630bd05a80b9/640b1db490971b1346e686fc_sc8-p-800.jpg" />
             </div>
             <div style={{ zIndex: 4, pointerEvents: "none", position: "absolute", height: "100%", transform: "translate(-50%)", left: `calc(50% + (${cardsDimension}px * 0.49))` }}>
-                <img alt='#' onClick={() => {
+                <img onMouseEnter={() => {
+                    if (document.getElementsByClassName('header_lines_btn')[0].style.background === "black") {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+                    } else {
+                        document.getElementsByClassName("primary-cursor")[0].style.width = "80px"
+                        document.getElementsByClassName("cursor-click")[0].textContent = "Click"
+                        document.getElementsByClassName("cursor-click")[0].style.color = "black"
+                        document.getElementsByClassName("primary-cursor")[0].style.height = "80px"
+                        document.getElementsByClassName("primary-cursor")[0].style.opacity = 0.9
+                        document.getElementsByClassName("cursor-click")[0].style.visibility = "visible"
+
+                    }
+                }} onMouseLeave={() => {
+                    document.getElementsByClassName("primary-cursor")[0].style.width = "15px"
+                    document.getElementsByClassName("cursor-click")[0].textContent = ""
+                    document.getElementsByClassName("cursor-click")[0].style.color = "white"
+                    document.getElementsByClassName("primary-cursor")[0].style.height = "12px"
+                    document.getElementsByClassName("primary-cursor")[0].style.opacity = 1
+                    document.getElementsByClassName("cursor-click")[0].style.visibility = "hidden"
+
+                }} alt='#' onClick={() => {
                     handleClick(4)
                 }} style={{
                     ...styles.cards, pointerEvents: "auto", objectFit: "cover", borderRadius: screen.mobile ? "1em" : "1.8em",
--- sharedComponents/customCursor/index.js
+++ sharedComponents/customCursor/index.js
@@ -1,39 +1,38 @@
 export const CustomCursor = ({ activeMouse }) => {
-    return (
+  return (
+    <div
+      className="primary-cursor"
+      style={{
+        position: "fixed",
+        width: "10px",
+        height: "10px",
+        borderRadius: "100%",
+        background: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 999,
+        left: 0,
+        top: 0,
+        display: activeMouse ? "block" : "none",
+        transition: "width 0.5s, height 0.5s, transform 0.1s",
+        pointerEvents: "none", overflow: "hidden"
+      }}
+    >
       <div
-        className="primary-cursor"
+        className="cursor-click"
         style={{
-          position: "fixed",
-          width: "10px",
-          height: "10px",
-          borderRadius: "100%",
-          background: "black",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          zIndex: 999,
-          left: 0,
-          top: 0,
-          display: activeMouse ? "block" : "none",
-          transition: "width 0.5s, height 0.5s, transform 0.1s",
+          textAlign: "center",
           pointerEvents: "none",
+          fontSize: "12px",
+          color: "white",
+          transform: "translate(-50%,-50%)",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          visibility: "hidden",
         }}
-      >
-        <div
-          className="cursor-click"
-          style={{
-            textAlign: "center",
-            pointerEvents: "none",
-            fontSize: "12px",
-            color: "white",
-            transform: "translate(-50%,-50%)",
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            visibility: "hidden",
-          }}
-        />
-      </div>
-    );
-  };
-  
+      />
+    </div>
+  );
+};
