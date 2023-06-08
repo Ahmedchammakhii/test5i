d479ba187f2b14279d59b3601caa96d1ac32432f
Chaabane fares <117437576+ChaabaneFares@users.noreply.github.com>
2023-06-08 14:11:49 +0100

    respon

--- pages/landing/hero/hero.js
+++ pages/landing/hero/hero.js
@@ -1,89 +1,128 @@
-import { useRef, useEffect, useState } from 'react'
-import data from "@/data/landing/projects"
-import words from "@/data/landing/hero"
+import { useRef, useEffect, useState } from "react";
+import data from "@/data/landing/projects";
+import words from "@/data/landing/hero";
 
 export default function Hero({ scroll }) {
-    const hero = useRef(null);
-    const animatedWords = useRef(null);
-    const veilNoise = useRef(null);
-    let counter = 0
-    let rebelot;
+  const hero = useRef(null);
+  const animatedWords = useRef(null);
+  const veilNoise = useRef(null);
+  let counter = 0;
+  let rebelot;
 
+  useEffect(() => {
+    if (
+      hero.current.offsetTop + hero.current.clientHeight * 0.4 >= scroll &&
+      document.querySelector("html").style.background !==
+        "rgba(241, 223, 213, 0.5)"
+    ) {
+      document.querySelector("html").style.background =
+        "rgba(241, 223, 213, 0.5)";
+      veilNoise.current.style.opacity = 1;
+      document.getElementsByClassName("primary-cursor")[0].style.background =
+        "linear-gradient(to bottom right, transparent, #3398ff 80%)";
+    } else if (
+      hero.current.offsetTop + hero.current.clientHeight * 0.4 < scroll &&
+      hero.current.offsetTop + hero.current.clientHeight * 0.5 >= scroll &&
+      document.querySelector("html").style.background !== data[0].background
+    ) {
+      document.querySelector("html").style.background = data[0].background;
 
-    useEffect(() => {
-        if (hero.current.offsetTop + (hero.current.clientHeight * 0.4) >= scroll && document.querySelector('html').style.background !== "rgba(241, 223, 213, 0.5)") {
+      veilNoise.current.style.opacity = 0;
+      document.getElementsByClassName("primary-cursor")[0].style.background =
+        data[0].color;
+    }
+  }, [scroll]);
+  return (
+    <div
+      ref={hero}
+      style={{
+        width: "90vw",
+        marginLeft: "5vw",
+        height: "90vh",
+        background: "transparent",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="veilNoise"
+        ref={veilNoise}
+        style={{
+          background:
+            "url(https://uploads-ssl.webflow.com/62e3ee10882dc50bcae8d07a/631a5d4631d4c55a475f3e34_noise-50.png)",
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          top: "0",
+          transition: "opacity 1s",
+          pointerEvents: "none",
+        }}
+      />
 
-            document.querySelector('html').style.background = "rgba(241, 223, 213, 0.5)";
-            veilNoise.current.style.opacity = 1
-            document.getElementsByClassName("primary-cursor")[0].style.background = "linear-gradient(to bottom right, transparent, #3398ff 80%)"
+      <div className="box">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-        } else if (hero.current.offsetTop + (hero.current.clientHeight * 0.4) < scroll && hero.current.offsetTop + (hero.current.clientHeight * 0.5) >= scroll && document.querySelector('html').style.background !== data[0].background) {
-            document.querySelector('html').style.background = data[0].background;
-
-            veilNoise.current.style.opacity = 0
-            document.getElementsByClassName("primary-cursor")[0].style.background = data[0].color
-        }
-    }, [scroll])
-    return (
-        <div ref={hero} style={{ width: "90vw", marginLeft: "5vw", height: "90vh", background: "transparent", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <div className='veilNoise' ref={veilNoise} style={{
-                background: "url(https://uploads-ssl.webflow.com/62e3ee10882dc50bcae8d07a/631a5d4631d4c55a475f3e34_noise-50.png)",
-                width: "100vw",
-                height: "100vh",
-                position: "fixed",
-                top: "0",
-                transition: "opacity 1s",
-                pointerEvents: "none"
-            }}
-            />
-
-            <div className="box">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-
-            <div style={{ fontWeight: "600", fontSize: "2.5em", maxWidth: "900px", textAlign: "center", lineHeight: "1.2", marginTop: "20vh" }} >
-                A branding agency with a twist
-            </div>
-            <div style={{
-                fontSize: "0.6em",
-                height: "4em",
-                position: "relative",
-                overflow: "hidden",
-                maxWidth: "800px",
-
-            }}>
-                <div onAnimationIteration={(event) => {
-                    if (counter === 0 && rebelot) {
-                        event.target.children[0].textContent = words[0]
-                        event.target.children[1].textContent = words[1]
-                        rebelot = false
-                    }
-                    else if (counter + 1 >= words.length - 1) {
-                        counter = 0
-                        event.target.children[0].textContent = words[words.length - 1]
-                        event.target.children[1].textContent = words[0]
-                        rebelot = true
-
-                    } else {
-                        counter++
-                        event.target.children[0].textContent = words[counter]
-                        event.target.children[1].textContent = words[counter + 1]
-                    };
-
-
-                }} ref={animatedWords} style={{
-                    position: "relative",
-                    top: "25%",
-                    textAlign: "center", height: "200%", animation: "translateYAnimation 5s infinite",
-
-                }}>
-                    <div style={{ height: "50%", fontWeight: "300" }}>{words[counter]}</div>
-                    <div style={{ height: "50%", fontWeight: "300" }}>{words[counter + 1]}</div>
-                </div>
-            </div>
-            <style>{`
+      <div
+        style={{
+          fontWeight: "600",
+          fontSize: "2.5em",
+          maxWidth: "900px",
+          textAlign: "center",
+          lineHeight: "1.2",
+          marginTop: "20vh",
+        }}
+      >
+        A branding agency with a twist
+      </div>
+      <div
+        style={{
+          fontSize: "0.6em",
+          height: "4em",
+          position: "relative",
+          overflow: "hidden",
+          maxWidth: "800px",
+        }}
+      >
+        <div
+          onAnimationIteration={(event) => {
+            if (counter === 0 && rebelot) {
+              event.target.children[0].textContent = words[0];
+              event.target.children[1].textContent = words[1];
+              rebelot = false;
+            } else if (counter + 1 >= words.length - 1) {
+              counter = 0;
+              event.target.children[0].textContent = words[words.length - 1];
+              event.target.children[1].textContent = words[0];
+              rebelot = true;
+            } else {
+              counter++;
+              event.target.children[0].textContent = words[counter];
+              event.target.children[1].textContent = words[counter + 1];
+            }
+          }}
+          ref={animatedWords}
+          style={{
+            position: "relative",
+            top: "25%",
+            textAlign: "center",
+            height: "200%",
+            animation: "translateYAnimation 5s infinite",
+          }}
+        >
+          <div style={{ height: "50%", fontWeight: "300" }}>
+            {words[counter]}
+          </div>
+          <div style={{ height: "50%", fontWeight: "300" }}>
+            {words[counter + 1]}
+          </div>
+        </div>
+      </div>
+      <style>{`
             @keyframes translateYAnimation {
                 0% {
                   transform: translateY(0);
@@ -142,7 +181,6 @@ export default function Hero({ scroll }) {
                 }
             }
             `}</style>
-        </div>
-    )
+    </div>
+  );
 }
-
