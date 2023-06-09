d0050e219cce3aaf221010393ffaa85ec61fc1cf
ahmedchammakhii <ahmedchammakhi2@gmail.com>
2023-06-09 16:21:12 +0100

    last commit

--- pages/services/fourthSection.jsx
+++ pages/services/fourthSection.jsx
@@ -131,31 +131,30 @@ const FourthSection = () => {
             degX +
             "deg) translateZ(-100px)");
       });
-
-      return () => {
-        document.removeEventListener("mousemove", function () {
-          mouse.x = event.clientX;
-          mouse.y = event.clientY;
-
-          posX =
-            ((mouse.x - window.innerWidth / 2) / (window.innerWidth / 2)) * 100;
-          posY =
-            ((mouse.y - window.innerHeight / 2) / (window.innerHeight / 2)) *
-            100;
-
-          degX = posX / sensibility;
-          degY = posY / sensibility;
-
-          packs.current.style &&
-            (packs.current.style.transform =
-              "rotateX(" +
-              -degY +
-              "deg) rotateY(" +
-              degX +
-              "deg) translateZ(-100px)");
-        });
-      };
     }
+
+    return () => {
+      document.removeEventListener("mousemove", function () {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+
+        posX =
+          ((mouse.x - window.innerWidth / 2) / (window.innerWidth / 2)) * 100;
+        posY =
+          ((mouse.y - window.innerHeight / 2) / (window.innerHeight / 2)) * 100;
+
+        degX = posX / sensibility;
+        degY = posY / sensibility;
+
+        packs.current.style &&
+          (packs.current.style.transform =
+            "rotateX(" +
+            -degY +
+            "deg) rotateY(" +
+            degX +
+            "deg) translateZ(-100px)");
+      });
+    };
   }, []);
   return (
     <div className={style.fourth} ref={sectionRef}>
