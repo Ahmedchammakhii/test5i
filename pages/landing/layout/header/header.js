1f362a2313b1cabf497e9a57efcf35ca518a9b43
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-06-13 15:10:24 +0100

    menu links mouse interactions

--- pages/landing/layout/header/header.js
+++ pages/landing/layout/header/header.js
@@ -1,13 +1,12 @@
 
 import { useCallback } from "react"
 import { headerMouseEnter, headerMouseLeave } from "../../../../functions/mouse";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import Menu from "./menu/menu";
 export default function Navbar({ isClicked, setIsClicked, scroll }) {
 
   const mousE = useCallback(headerMouseEnter, [])
   const mouseL = useCallback(headerMouseLeave, [])
-  const router = useRouter()
 
 
   return (
@@ -40,38 +39,7 @@ export default function Navbar({ isClicked, setIsClicked, scroll }) {
       </div>
 
 
-      <div className="G_father_container" style={{ zIndex: 1000, pointerEvents: isClicked ? "auto" : "none" }}>
-        <div className="father_container" style={{ width: isClicked ? "200%" : "0", height: isClicked ? "500%" : "0" }}>
-
-          <div className="container" style={{ opacity: isClicked ? 1 : 0, transition: "opacity 0.5s", position: "absolute", left: "-25%" }}>
-            <div className="menu">
-
-              <div className="left_side">
-                <div className="menu_title" >get in touch</div>
-                <div>info@info.com</div>
-                <div className="menu_title" style={{ marginTop: "30px" }}>social</div>
-                <div>facebook</div>
-                <div>instagram</div>
-                <div>linkedin</div>
-                <div>github</div>
-              </div>
-
-              <div className="right_side" style={{ fontSize: "2.4rem" }}>
-                <div className="menu_title" >menu</div>
-                <div onClick={() => router.push("/services")} style={{ fontSize: "1.26rem" }} onMouseEnter={(e) => e.target.style.color = "black"} onMouseLeave={(e) => e.target.style.color = "#6e6e73"}>Services</div>
-                <div onClick={() => router.push("/blogs")} style={{ fontSize: "1.26rem" }} onMouseEnter={(e) => e.target.style.color = "black"} onMouseLeave={(e) => e.target.style.color = "#6e6e73"}>Blogs</div>
-                <div onClick={() => router.push("/contact")} style={{ fontSize: "1.26rem" }} onMouseEnter={(e) => e.target.style.color = "black"} onMouseLeave={(e) => e.target.style.color = "#6e6e73"}>Contact</div>
-                <div onClick={() => router.push("/choices")} style={{ fontSize: "1.26rem" }} onMouseEnter={(e) => e.target.style.color = "black"} onMouseLeave={(e) => e.target.style.color = "#6e6e73"}>Catalogues</div>
-                <div className="mobile_button">
-                  Estimate Project
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-        </div>
-      </div>
+      <Menu isClicked={isClicked} />
 
       <style>{`
           .main_container {z-index: 1111; margin-left: 3.6%; margin-right: 3.6%; display: flex; justify-content: space-between; position: fixed; top: 0; width: calc(100% - (3.6% * 2)); overflow: hidden; }
@@ -82,17 +50,7 @@ export default function Navbar({ isClicked, setIsClicked, scroll }) {
           .quote { cursor: pointer; width: 100px; height: 40px; background: linear-gradient(to bottom right, transparent, #3398ff 80%); border-radius: 50px; color: white; font-size: 13px; display: flex; justify-content: center; align-items: center; font-weight: 600; transition: 0.5s;} 
           .lines_btn {width: 50px;height: 100%;flex-direction: column;display: flex;justify-content: center;cursor: pointer;z-index: 1002} 
           .header_lines_btn { background: black; height: 3px; margin-left: 19px; margin-right: 5px; border-radius: 50px; transition: transform 0.5s, background 2s; } 
-          .G_father_container { height: calc(100vh + 80px); width: 100%; margin-top: -80px; overflow: hidden; position: fixed; background: transparent; }
-          .father_container { background: white; transition: 0.6s; float: right; border-bottom-left-radius: 100% } 
-          .container { width: 50%; margin-left: 50%; height: calc(100vh + 80px); margin-top: 80px; display: flex; justify-content: center; align-items: center }
-          .menu { width: 50%; display: flex; justify-content: center; gap: 170px; }
-          .menu_title { font-size: 0.38rem; font-weight: 500;}
-          .left_side {  cursor: pointer; font-size: 0.42rem;color: #6e6e73;font-weight: 600;line-height: 3;margin-top: -20%;}
-          .right_side { cursor: pointer;  font-weight: 500; color: #6e6e73; line-height: 1.8; margin-top: -20%;}
-          .mobile_button { background-color: #3398ff; color: white; border-radius: 50px; font-size: 20px; width: 80vw; height: 60px;  align-items: center; justify-content: center; margin-top: 60px; display : none; }
-
-
-
+          
 
 
 
